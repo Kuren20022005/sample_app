781a01554706df4a34f264c0405921646ce8141e
lamnh <kuren99889977@gmail.com>
2026-07-29 09:43:05 +0700

    Chapter 7

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
+  get 'users/show'
   get "static_pages/home"
   get "static_pages/help"
+
+  get "/signup", to: "users#new"
+  post "/signup", to: "users#create"
+  resources :users, only: %i(new create show)
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
